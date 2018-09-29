57562f38b5f8c2bc714e29095ebfdc14c479715e
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-09-29 00:25:49 -0400

    adding the changes to the template model

---
+++ assets/js/functions.js
@@ -0,0 +1,14 @@
+
+window.onload = function () {
+
+
+
+$( document ).ready(function() {
+    $('.button').on('click', function(){
+  console.log('this is clicking');
+  
+    })
+    // Get started!
+  
+  });
+};
