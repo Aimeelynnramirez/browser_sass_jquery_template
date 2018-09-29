936f0723a598e4d58316e3a6828e9145d0435467
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-09-29 05:25:40 -0400

    fizzbuzz example merge

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,14 @@
 
 window.onload = function () {
 $( document ).ready(function() {
+	//this is checking if the buttons are clicking in call.
     $('.button').on('click', function(){
-  //console.log('this is clicking');
+  console.log(' these buttons are clicking');
   
     })
   
   });
-
-
-	
-	
-	const merge = $('.button').on('click', function(){
+	const merge = $('.fruitButton').on('click', function(){
      //this is name of the array initally "basketOne.length"
 	const basketOne = ['pineapples','pears','tomatoes'];
 	//console.log('this is first array of types:', basketOne);
@@ -37,3 +34,21 @@ $( document ).ready(function() {
 	}
 	})
 };
+
+const mergeFizzBuzz = $('.fizzBuzzButton').on('click', function(){
+	//this is an example of a length of 3 times to loop with 1 and 2 being the string values"
+	let fizz = ['1'];
+	let buzz = ['2'];
+	let f = fizz  || buzz;
+	for (f = 1 ||  2; f <= 3; f++) {
+   	(fizz === 1) 
+   console.log('this is fizz:', fizz[0]);
+   	(buzz === 2) 
+	console.log('this is buzz:', buzz[0]);
+	let fizzBuzz = fizz[0] + ','+ buzz[0];
+	console.log('this is fizzbuzz in a list:', fizzBuzz);
+	console.log('this is adding values of fizzbuzz:', fizzBuzz.length);	
+}
+//return mergeFizzBuzz;
+})
+
